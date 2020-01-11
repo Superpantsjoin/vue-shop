bcaa929853ac22bd255503c66443a224220a490f
peter <781583484@qq.com>
2020-01-11 19:30:08 +0800

    fix Home.vue

--- vue-shop/src/components/Home.vue
+++ vue-shop/src/components/Home.vue
@@ -4,7 +4,7 @@
             <el-button type="info" @click="exit">退出</el-button>
         </el-header>
         <el-container>
-            <el-aside width="200px">Aside</el-aside>
+            <el-aside width="200px">Aside1</el-aside>
             <el-main>Main</el-main>
         </el-container>
     </el-container>
