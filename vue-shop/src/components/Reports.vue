31f35dd94ab1bad94cf2c4b266cb89b79871166a
peter <781583484@qq.com>
2020-02-25 17:12:07 +0800

    will finished reports

--- vue-shop/src/components/Reports.vue
+++ vue-shop/src/components/Reports.vue
@@ -1,24 +1,138 @@
 <template>
-    <h3>数据报表组件</h3>
+    <div>
+        <!-- 导航条 -->
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/home/welcome' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+            <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <!-- 卡片视图 -->
+        <el-card>
+            <div id="main" style="width: 750px; height: 400px"></div>
+        </el-card>
+    </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
     name: 'reports',
     data() {
         return {
-            
+            option: {
+                title: {
+                    text: '用户来源'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        label: {
+                            backgroundColor: '#6a7985'
+                        }
+                    }
+                },
+                legend: {
+                    data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        boundaryGap: false,
+                        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value'
+                    }
+                ],
+                series: [
+                    {
+                        name: '邮件营销',
+                        type: 'line',
+                        stack: '总量',
+                        areaStyle: {},
+                        data: [120, 132, 101, 134, 90, 230, 210]
+                    },
+                    {
+                        name: '联盟广告',
+                        type: 'line',
+                        stack: '总量',
+                        areaStyle: {},
+                        data: [220, 182, 191, 234, 290, 330, 310]
+                    },
+                    {
+                        name: '视频广告',
+                        type: 'line',
+                        stack: '总量',
+                        areaStyle: {},
+                        data: [150, 232, 201, 154, 190, 330, 410]
+                    },
+                    {
+                        name: '直接访问',
+                        type: 'line',
+                        stack: '总量',
+                        areaStyle: {},
+                        data: [320, 332, 301, 334, 390, 330, 320]
+                    },
+                    {
+                        name: '搜索引擎',
+                        type: 'line',
+                        stack: '总量',
+                        label: {
+                            normal: {
+                                show: true,
+                                position: 'top'
+                            }
+                        },
+                        areaStyle: {},
+                        data: [820, 932, 901, 934, 1290, 1330, 1320]
+                    }
+                ]
+            }
         }
     },
     methods: {
 
     },
-    created() {
-        
+    async mounted() {
+        var myChart = echarts.init(document.getElementById('main'));
+
+        var option = {};
+
+        const resp = await this.$http.get('reports/type/1');
+        if(resp.data.meta.status === 200) {
+            option = resp.data.data;
+        } else {
+            this.$message.error(resp.data.meta.msg);
+        }
+
+        console.log(option);
+
+        myChart.setOption(this.option);
     }
 }
 </script>
 
 <style scoped>
-
+.el-breadcrumb {
+    margin-bottom: 15px;
+    font-size: 12px;
+}
+.el-card {
+    box-shadow: 0 0 0 rgba(0, 0, 0, 0.15);
+}
 </style>
